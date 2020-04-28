73243b66b0138fe53f5f02d0ebcf7e015731d4b5
rey <rey.echeverria7@gmail.com>
2020-04-28 00:49:34 -0700

    Initial commit

---
+++ app/src/main/java/echeverria/reynaldo/misnotas/AgregarNotaActivity.kt
@@ -0,0 +1,87 @@
+package echeverria.reynaldo.misnotas
+
+import android.content.pm.PackageManager
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Environment
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import kotlinx.android.synthetic.main.activity_agregar_nota.*
+import java.io.File
+import java.io.FileOutputStream
+import java.util.jar.Manifest
+
+
+class AgregarNotaActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_agregar_nota)
+
+        btn_guardar.setOnClickListener{
+            guardar_nota();
+        }
+    }
+
+    fun guardar_nota(){
+        if(ContextCompat.checkSelfPermission(this, android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+            != PackageManager.PERMISSION_GRANTED
+        ){
+
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(android.Manifest.permission.WRITE_EXTERNAL_STORAGE),
+                235
+
+            )
+
+        }else{
+            guardar()
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        when (requestCode){
+            235 -> {
+                if((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)){
+                    guardar()
+                }else{
+                    Toast.makeText(this,"Error: permisos denegados",Toast.LENGTH_SHORT).show()
+                }
+
+            }
+        }
+    }
+
+    fun guardar(){
+        var titulo = et_titulo.text.toString()
+        var cuerpo = et_contenido.text.toString()
+        if(titulo == "" || cuerpo == ""){
+            Toast.makeText(this,"Error: campos vacíos",Toast.LENGTH_SHORT).show()
+        }else{
+            try{
+                val archivo = File(ubicacion(),titulo + ".txt")
+                val fos = FileOutputStream(archivo)
+                fos.write(cuerpo.toByteArray())
+                fos.close()
+                Toast.makeText(
+                    this,
+                    "Se guardó el archivo en la carpeta pública",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }catch (e: Exception){
+                Toast.makeText(this,"Error: no se guardó el archivo",Toast.LENGTH_SHORT).show()
+            }
+        }
+        finish()
+    }
+
+    private fun ubicacion():String{
+        val carpeta = File(Environment.getExternalStorageDirectory(),"notas")
+        if(!carpeta.exists()){
+            carpeta.mkdir()
+        }
+        return carpeta.absolutePath
+    }
+}
---
+++ app/src/main/java/echeverria/reynaldo/misnotas/Nota.kt
@@ -0,0 +1,3 @@
+package echeverria.reynaldo.misnotas
+
+class Nota(var titulo:String,var contenido:String) {}
